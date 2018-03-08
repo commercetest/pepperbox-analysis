d8fc7a1d80fe6f941e1a712f3069b3ed24888045
Joe Reeve <joseph.reeve@googlemail.com>
2018-03-08 12:51:35 +0000

    Updated script

--- extractResults.sh
+++ extractResults.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-for filename in $( ls $1 ); do
-    sortedFile="${filename}.sorted"
-    outputFile="$filename.result.csv"
+globalOut="out/general.csv"
+
+if [ -f "$globalOut" ]; then
+    echo "Found previous general results file [$globalOut] DELETING - you have 5 seconds to cancel"
+    sleep 5
+    rm $globalOut
+fi
+
+echo "originFileName,consumerLagMean" >> $globalOut
+
+for file in $( ls $1 ); do
+    filename=${file/'.csv'/''}
+    sortedFile="${filename}.sorted-by-consumerLag"
+    outputFile=$filename.result.csv
+    consumerLagColumn=3
 
     if [ -f "$outputFile" ]; then
         echo "Found previous results file [$outputFile] DELETING - you have 5 seconds to cancel"
@@ -10,18 +22,18 @@ for filename in $( ls $1 ); do
         rm $outputFile
     fi
 
-    head -1 $filename > $outputFile
+    head -1 $file > $outputFile
 
     if [ ! -f "$sortedFile" ]; then
-        echo "Sorted file not found for [$filename] - generating now"
-        tail -n +2 $filename | sort -t "," -k 3 -g >> $sortedFile
+        echo "Sorted file not found for [$file] - generating now"
+        tail -n +2 $file | sort -t "," -k $consumerLagColumn -g >> $sortedFile
     else
-        echo "Found previously sorted file [$filename]"
+        echo "Found previously sorted file [$file]"
     fi
 
     linecountStr=$(wc -l < $sortedFile)
     linecount=$(($linecountStr + 0))
-    echo "File [$sortedFile] has [$linecount] lines";
+    echo "File [$sortedFile] has [$linecount] lines"
 
     for i in `seq 1 100`;
     do
@@ -31,6 +43,13 @@ for filename in $( ls $1 ); do
         echo $tmp >> $outputFile
     done
 
-    echo "Written results to [$outputFile]"
+    consumerLagMean=$(awk -F "," '{ total += $3 } END { print total/NR }' $file)
+    
+    echo "Consumer Lag Mean is [${consumerLagMean}]"
 
+    echo "${file},${consumerLagMean}" >> $globalOut
+
+    echo "Written results to [$outputFile]"
 done
+
+echo "Global results written to [$globalOut]"
